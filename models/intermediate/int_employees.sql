f9019a0811800f10a3e2feafc7b79d07804b07ac
Xixi Chen <cxxhelen357@gmail.com>
2026-04-18 01:24:39 +0000

    update script

--- models/intermediate/int_employees.sql
+++ models/intermediate/int_employees.sql
@@ -1,22 +1,22 @@
-WITH joins AS (
-    SELECT
-        CAST(employee_id AS VARCHAR) AS employee_id,
-        REPLACE(hire_date, 'day ', '')::DATE AS hire_date,
-        name,
-        city,
-        address,
-        title,
-        CAST(annual_salary AS DOUBLE) AS annual_salary
-    FROM DEV.XINAN_BASE.BASE_GOOGLE__HR_JOINS),
+-- WITH joins AS (
+--     SELECT
+--         CAST(employee_id AS VARCHAR) AS employee_id,
+--         REPLACE(hire_date, 'day ', '')::DATE AS hire_date,
+--         name,
+--         city,
+--         address,
+--         title,
+--         CAST(annual_salary AS DOUBLE) AS annual_salary
+--     FROM DEV.XINAN_BASE.BASE_GOOGLE__HR_JOINS),
 
-quits AS (
-    SELECT
-        CAST(employee_id AS VARCHAR) AS employee_id,
-        TRY_TO_DATE(quit_date) AS quit_date
-    FROM {{ source('google_drive', 'HR_QUITS') }}
-    WHERE TRY_TO_DATE(quit_date) IS NOT NULL
-        AND employee_id IS NOT NULL
-)
+-- quits AS (
+--     SELECT
+--         CAST(employee_id AS VARCHAR) AS employee_id,
+--         TRY_TO_DATE(quit_date) AS quit_date
+--     FROM {{ source('google_drive', 'HR_QUITS') }}
+--     WHERE TRY_TO_DATE(quit_date) IS NOT NULL
+--         AND employee_id IS NOT NULL
+-- )
 
 SELECT
     j.employee_id,
@@ -27,6 +27,6 @@ SELECT
     j.title,
     j.annual_salary,
     q.quit_date
-FROM joins j
-LEFT JOIN quits q
+FROM DEV.XINAN_BASE.BASE_GOOGLE__HR_JOINS j
+LEFT JOIN {{ source('google_drive', 'HR_QUITS') }} q
     ON j.employee_id = q.employee_id
