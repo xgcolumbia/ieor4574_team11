e92e0faa665f38eafd0f2f2f2190143670243490
Xinan GE <calen2458@gmail.com>
2026-04-25 19:54:04 +0000

    update fact_order

--- models/analytics/fact_order.sql
+++ models/analytics/fact_order.sql
@@ -1,24 +1,27 @@
--- {{ config(materialized='view') }}
+{{ config(materialized='view') }}
+
 SELECT
     order_id,
     session_id,
     client_name,
     order_ts,
-    CAST(order_ts AS DATE) AS order_date,
+    CAST(order_ts AS DATE)                    AS order_date,
     payment_method,
     shipping_cost,
     tax_rate,
     state,
+    gross_item_revenue,
+    revenue_before_tax,
+    total_revenue,
     returned_at,
     CASE
         WHEN returned_at IS NOT NULL THEN TRUE
         ELSE FALSE
-    END AS has_returned_flag,
+    END                                       AS has_returned_flag,
     is_refunded,
     CASE
         WHEN returned_at IS NOT NULL
         THEN DATEDIFF('day', CAST(order_ts AS DATE), returned_at)
         ELSE NULL
-    END AS return_days_after_order
+    END                                       AS return_days_after_order
 FROM {{ ref('int_fact_order') }}
-
