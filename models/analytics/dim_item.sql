e27f8aa1c486d253df5b8a8178510eb359fc7e90
Meixu Wang <sabrinawang2003@gmail.com>
2026-04-26 23:29:02 +0000

    fixed error

--- models/analytics/dim_item.sql
+++ models/analytics/dim_item.sql
@@ -2,8 +2,7 @@
 SELECT
     MD5(item_name) AS item_id,
     item_name,
-    MAX(price_per_unit) AS price_per_unit,
-    session_id AS session_id
+    MAX(price_per_unit) AS price_per_unit
 FROM {{ ref('int_item') }}
 WHERE item_name IS NOT NULL
 GROUP BY item_name
